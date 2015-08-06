cdca10fa381f062c612fc3553a49e44124b952b5
Visal Sean <sean.visal@gmail.com>
2015-08-06 07:55:36 +0700

    Adding a new project for testing keyword static

---
+++ StaticKeywordTest/src/statickeywordtest/ClientTest.java
@@ -0,0 +1,25 @@
+package statickeywordtest;
+
+
+/**
+ * Created by Visal Sean.
+ */
+public class ClientTest {
+
+    public static void main(String[] args) {
+        QuietStatic ex1 = new QuietStatic();
+        ex1.classMember++;
+        ex1.instanceMember++;
+
+
+        QuietStatic ex2 = new QuietStatic();
+        ex2.classMember++;
+        ex2.instanceMember++;
+
+
+        System.out.println("ex1 class member: " + ex1.classMember);
+        System.out.println("ex1 instance member: " + ex1.instanceMember);
+        System.out.println("ex2 class member: " + ex2.classMember);
+        System.out.println("ex2 instance member: " + ex2.instanceMember);
+    }
+}
---
+++ StaticKeywordTest/src/statickeywordtest/QuietStatic.java
@@ -0,0 +1,12 @@
+package statickeywordtest;
+
+
+/**
+ * Created by Visal Sean.
+ */
+public class QuietStatic {
+
+    public static int classMember;
+    public int instanceMember;
+
+}
